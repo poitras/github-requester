62e4b272e86e4816b3a1051745a0fdb9ef9f0e32
poitras <nicopoitras@gmail.com>
2017-12-11 23:24:05 -0500

    Added github request and info in UI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 import './App.css'
 
 class App extends Component {
 
   constructor () {
     super()
+    this.state = {
+      username: '',
+      name: '',
+      avatar_url: ''
+    }
 
     this.handleClick = this.handleClick.bind(this)
   }
 
   handleClick () {
-    console.log('Click');
-  } 
+    console.log('Click')
+    axios.get('https://api.github.com/users/poitras')
+      .then(response => {
+        console.log('>>>>', response);
+        this.setState({
+          username: response.data.login,
+          name: response.data.name,
+          avatar_url: response.data.avatar_url
+        })
+      })
+  }
 
   render () {
     return (
@@ -19,6 +35,9 @@ class App extends Component {
         <button className='button' onClick={this.handleClick}>
           Click Me
         </button>
+        {this.state.username ? <p>username: {this.state.username}</p> : null}
+        {this.state.name ? <p>name: {this.state.name}</p> : null}
+        {this.state.avatar_url ? <img src={this.state.avatar_url} alt="avatar" height="100" width="100" /> : null}
       </div>
     )
   }
